c2510b9cf6d429c709b1fbd84784105594447e58
Renan <renanandreolla@outlook.com>
2024-07-23 15:00:53 -0300

    feat: update

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// const corsOptions = {
-//     origin: ['http://localhost:3030', 'https://dema-automotive-95e44bfb4ebf.herokuapp.com/'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// };
-  
-app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://dema-automotive-95e44bfb4ebf.herokuapp.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 const database_init = require('./src/database/mongodb');
 
@@ -20,7 +19,7 @@ database_init();
 
 app.use("/dema", require("./src/server"));
 
-const port = 3030;
+const port = 3000;
 
 app.listen(port, () => {
     console.log("Listen on port: " + port);
