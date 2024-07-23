f514bbae03e71a2b4925309a17ce0aa21aca0435
Renan <renanandreolla@outlook.com>
2024-07-23 14:31:23 -0300

    feat: adjust CORS

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://dema-automotive-95e44bfb4ebf.herokuapp.com/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+  
+app.use(cors(corsOptions));
 
 const database_init = require('./src/database/mongodb');
 
