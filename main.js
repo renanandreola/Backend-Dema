83e08f0e772e16dbcc4b2e07cc4344a69ab2fd8d
Renan <renanandreolla@outlook.com>
2024-07-23 18:40:46 -0300

    feat: env port Heroku

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://dema-automotive-95e44bfb4ebf.herokuapp.com/');
+    res.header('Access-Control-Allow-Origin', 'https://dema-automotive-95e44bfb4ebf.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+app.use(cors({
+    origin: 'https://dema-automotive-95e44bfb4ebf.herokuapp.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const database_init = require('./src/database/mongodb');
 
@@ -19,7 +25,7 @@ database_init();
 
 app.use("/dema", require("./src/server"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Listen on port: " + port);
