13f424418b26ba506ee815194c482b2aa396ac5b
Renan <renanandreolla@outlook.com>
2024-07-23 19:01:41 -0300

    feat: get products test

--- src/database/operations/getProducts.js
+++ src/database/operations/getProducts.js
@@ -3,30 +3,26 @@ const ACCESS_DB = require('../../config/envDB')
 
 const URI = "mongodb+srv://" + ACCESS_DB.DB_Credentials.Username + ":" + ACCESS_DB.DB_Credentials.Password + "@demacluster.9yaczoz.mongodb.net/Dema_Database?retryWrites=true&w=majority";
 
+let client;
+
+async function connectToDatabase() {
+    if (!client) {
+        client = new MongoClient(URI, { useUnifiedTopology: true });
+        await client.connect();
+    }
+    return client.db('products');
+}
+
 async function getProducts() {
-    return new Promise(async (resolve, reject) => {
-        const client = new MongoClient(URI, { 
-            useUnifiedTopology: true
-        });
-
-        try {
-            await client.connect();
-            
-            const database = client.db('products');
-            const collectionProducts = database.collection('products');
-
-            const result = await collectionProducts.find().toArray();
-   
-            resolve(result);
-
-        } catch (error) {
-            console.log('Error on get products: ', error);
-            reject(error);
-
-        } finally {
-            await client.close();
-        }
-    });
+    try {
+        const database = await connectToDatabase();
+        const collectionProducts = database.collection('products');
+        const result = await collectionProducts.find().toArray();
+        return result;
+    } catch (error) {
+        console.log('Error on get products: ', error);
+        throw error;
+    }
 }
 
 module.exports = getProducts;
