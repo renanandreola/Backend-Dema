9426853e305c6327c58e81c4d991552a44e8a35a
Renan <renanandreolla@outlook.com>
2024-07-23 14:24:38 -0300

    feat: adjust main

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
-app.use(express.json());
 
 const database_init = require('./src/database/mongodb');
 
