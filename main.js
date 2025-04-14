8dcce6bf4047da74c1659512bac50b3f8e7fd32e
Renan <renanandreolla@outlook.com>
2025-04-13 23:29:14 -0300

    up vercel

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// Comment on localhost
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://dema-automotive-95e44bfb4ebf.herokuapp.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://demautomotive.vercel.app/"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use(cors({
-    origin: 'https://dema-automotive-95e44bfb4ebf.herokuapp.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3001",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
-const database_init = require('./src/database/mongodb');
+const database_init = require("./src/database/mongodb");
 
 database_init();
 
@@ -29,5 +33,5 @@ app.use("/dema", require("./src/server"));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Listen on port: " + port);
+  console.log("Listen on port: " + port);
 });
